9b36fc598188059d8a076a1b8c418d35910e21c8
Peter Lesslie <pclesslie@gmail.com>
2016-05-11 22:09:49 -0600

    Save work

--- parser.c
+++ parser.c
@@ -5,7 +5,17 @@
 #include "token.h"
 #include "object.h"
 
+enum {
+    ST_INIT,
+    ST_LIST,
+    ST_ERR,
+    ST_FIN
+};
+
+typedef unsigned char state_type_t;
+
 struct state_t {
+    state_type_t type;
 };
 
 int64_t parse_int(const char *beg, const char *end) {
@@ -21,6 +31,7 @@ int64_t parse_int(const char *beg, const char *end) {
 
 int parser_init(parser_t *parser) {
     struct state_t *state = malloc(sizeof(*state));
+    state->type = ST_INIT;
     *parser = (void*)state;
     return 0;
 }
@@ -38,12 +49,22 @@ int parse_term(struct state_t *state, struct token_t *token) {
             printf("Ident!\n");
             return 0;
         default:
+            state->type = ST_ERR;
             return 1;
     }
 }
 
 int parse_expr(struct state_t *state, struct token_t *token) {
-    return parse_term(state, token);
+    if (state->type == ST_ERR || state->type == ST_FIN) { // unnecessary?
+        return 1;
+    } else if (state->type == ST_LIST) {
+
+    } else if (token->type == LPAREN) {
+        state->type = ST_LIST;
+        return 0;
+    } else {
+        return parse_term(state, token);
+    }
 }
 
 int parser_parse(parser_t parser, struct token_t *token) {
