27bc5558492a5d450aea79b68d6c984489bea7d9
Peter Lesslie <pclesslie@gmail.com>
2018-01-06 21:29:00 -0600

    Lexer

--- pylisp.py
+++ pylisp.py
@@ -1,133 +1,115 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+from enum import Enum, auto
+
+class TokenType(Enum):
+    LPAREN = auto()
+    RPAREN = auto()
+    LBRACE = auto()
+    RBRACE = auto()
+    PLUS = auto()
+    MINUS = auto()
+    STAR = auto()
+    SLASH = auto()
+    STRING = auto()
+    NUMBER = auto()
+    IDENT = auto()
+    EOF = auto()
 
 
-TK_UNK = 0
-TK_LPAREN = 1
-TK_RPAREN = 2
-TK_PLUS = 3
-TK_NUMBER = 4
-TK_IDENT = 5
-TK_EOF = 6
-    
+class Token(object):
+    def __init__(self, ttype, lexeme):
+        # TODO: add linum
+        self.ttype = TokenType(ttype)
+        self.lexeme = str(lexeme)
 
-TokenString = {
-        TK_UNK: 'Unknown',
-        TK_LPAREN: 'LParen',
-        TK_RPAREN: 'RParen',
-        TK_PLUS: 'Plus',
-        TK_NUMBER: 'Number',
-        TK_IDENT: 'Ident',
-        TK_EOF: 'EOF',
-}
+    def __str__(self):
+        return '{ttype!s}({lexeme!s})'.format(
+                ttype=self.ttype, lexeme=self.lexeme)
 
 
 class Tokens(object):
-    def __init__(self, text):
-        self.text = text
-        self.mlen = len(self.text)
-        self.pos = 0
-        self.ttyp = TK_UNK
-        self.tval = ''
-        self.advance()
-        
-    def advance(self):
-        while self.pos < self.mlen and self.text[self.pos].isspace():
-            self.pos += 1
-        if not self.pos < self.mlen:
-            self.ttyp = TK_EOF
-            self.tval = ''
-            return
-        c = self.text[self.pos]
-        self.pos += 1
-        if c == '(':
-            self.ttyp = TK_LPAREN
-            self.tval = c
-        elif c == ')':
-            self.ttyp = TK_RPAREN
-            self.tval = c
-        elif c == '+':
-            self.ttyp = TK_PLUS
-            self.tval = c
-        elif c.isdigit():
-            self.ttyp = TK_NUMBER
-            self.tval = c
-            while self.pos < self.mlen and self.text[self.pos].isdigit():
-                self.tval += self.text[self.pos]
-                self.pos += 1
-        else:
-            self.ttyp = TK_IDENT
-            self.tval = c
-            while self.pos < self.mlen:
-                c = self.text[self.pos]
-                if not (c.isdigit() or c.isalpha() or c == '_'):
-                    break
-                self.tval += c 
-                self.pos += 1
+    def __init__(self, source):
+        self.src = source
+        self.cur = 0
+        self.end = len(source)
+        self.endtok = Token(TokenType.EOF, lexeme='EOF')
+        self.tok = self._next()
 
-    def match(self, typ):
-        if self.ttyp == typ:
-            self.advance()
+    def peek(self):
+        return self.tok.ttype
+
+    def expect(self, ttype, msg=None):
+        if self.peek() != ttype:
+            if msg is None:
+                msg = "Expected {!s}, instead received {!s}".format(
+                        ttype, self.peek())
+            raise ValueError(msg)
+        self.tok = self._next()
+
+    def match(self, ttype):
+        if self.peek() == ttype:
+            self.tok = self._next()
             return True
-        return False
-    
-    def expect(self, typ):
-        if self.ttyp != typ:
-            raise ValueError("Expected token of type {}, received {}".format(
-                    TokenString[typ], TokenString[self.ttyp]))
-        self.match(typ)
-    
-    
-class Number(object):
-    def __init__(self, val):
-        self.val = float(val)
-    
-    def __str__(self):
-        return str(self.val)
-        
-    
-class Plus(object):
-    def __init__(self, args):
-        self.args = args
-        
-    def __str__(self):
-        args = [str(x) for x in self.args]
-        return 'Plus({})'.format(' '.join(args))
-    
-    
-def expr(tokens):
-    tval = tokens.tval
-    if tokens.match(TK_LPAREN):
-        result = expr(tokens)
-        tokens.expect(TK_RPAREN)
-    elif tokens.match(TK_NUMBER):
-        result = Number(tval)
-    elif tokens.match(TK_PLUS):
-        args = []
-        while tokens.ttyp != TK_RPAREN:
-            args.append(expr(tokens))
-        result = Plus(args)
-    else:
-        raise ValueError("Invalid token: [{}] -> {}".format(
-                TokenString[tokens.ttyp], tokens.tval))
-    return result
+        else:
+            return False
 
-
-def parse(prog):
-    tokens = Tokens(prog)
-    while not tokens.match(TK_EOF):
-        result = expr(tokens)
-    return result
-    
-
-_Debug = False
-def classname(x):
-    if _Debug:
-        return str(type(x))
-    else:
-        return str(type(x)).split('.')[1][:-2] 
+    def _next(self):
+        i = self.cur
+        end = self.end
+        src = self.src
+        while i < end and src[i].isspace():
+            i += 1
+        if not i < end:
+            self.cur = i
+            return self.endtok
+        c = src[i]
+        i += 1
+        if c == '(':
+            result = Token(TokenType.LPAREN, c)
+        elif c == ')':
+            result = Token(TokenType.RPAREN, c)
+        elif c == '+':
+            result = Token(TokenType.PLUS, c)
+        elif c == '-':
+            result = Token(TokenType.MINUS, c)
+        elif c == '*':
+            result = Token(TokenType.STAR, c)
+        elif c == '/':
+            result = Token(TokenType.SLASH, c)
+        elif c == '"':
+            while i < end and src[i] != '"':
+                c += src[i]
+                i += 1
+            if src[i] != '"':
+                raise ValueError("Expect '\"' to close string.")
+            result = Token(TokenType.STRING, c)
+        elif c.isdigit():
+            while i < end and src[i].isdigit():
+                c += src[i]
+                i += 1
+            result = Token(TokenType.NUMBER, c)
+        elif c.isalpha():
+            # TODO: allow '_'?  maybe this should be "while not src[i].isspace()"?
+            while i < end and src[i].isalnum():
+                c += src[i]
+                i += 1
+            result = Token(TokenType.IDENT, c)
+        else:
+            raise ValueError("Unexpected character: '{}'".format(c))
+        self.cur = i
+        return result
 
 
 if __name__ == '__main__':
-    prog = "(+ 1 25 3)"
-    result = parse(prog)
-    print "{} -> {}".format(classname(result), result)
+    # tok = Token(TokenType.IDENT, "HelloWorld")
+    # print(str(tok))
+    source = "(+ 1 2)"
+    tokens = Tokens(source)
+    i = 0
+    while not tokens.match(TokenType.EOF) and i < 10:
+        print(tokens.tok)
+        tokens.tok = tokens._next()
+        i += 1
+    print(tokens.tok)
+
